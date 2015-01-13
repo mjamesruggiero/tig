0b90889977087180b2f3cf90bd8431939b7b5b07
mjamesruggiero <mjamesruggiero@gmail.com>
2015-01-12 17:26:36 -0800

    using aws 2-tier example

--- example.tf
+++ example.tf
@@ -4,6 +4,24 @@ provider "aws" {
     region = "${var.region}"
 }
 
+resource "aws_security_group" "default" {
+    name = "terraform_example"
+    description = "Used in the terraform"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
 resource "aws_eip" "ip" {
     instance = "${aws_instance.example.id}"
 }
@@ -12,9 +30,13 @@ resource "aws_instance" "example" {
     ami = "${lookup(var.amis, var.region)}"
     instance_type = "t1.micro"
 
-    provisioner "local-exec" {
-        command = "echo ${aws_instance.example.public_ip} > file.txt"
+    connection {
+        user = "ubuntu"
+        key_file = "${var.key_path}"
     }
+
+    key_name = "${var.key_name}"
+    security_groups = ["${aws_security_group.default.name}"]
 }
 
 output "ip" {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+variable "key_path" {}
+variable "key_name" {}
 variable "access_key" {}
 variable "secret_key" {}
 variable "region" {
